1ff338e2e70b2b68cd42d06fc4569f4449aeea3d
Otto Sanchez <otto.sancheza@gmail.com>
2020-01-05 11:07:54 -0600

    completes updating tests

--- __tests__/db.test.js
+++ __tests__/db.test.js
@@ -21,7 +21,7 @@ const knex = require('knex')({
 
 test('should return data from postgreSQL by recordNumber- Dummy Test', (done) => {
   return knex('relatedproducts')
-  .where({recordnumber: 9999090})
+  .where({recordnumber: 200})
   .then(result => {
     expect(result).toBeTruthy();
   })
@@ -30,34 +30,34 @@ test('should return data from postgreSQL by recordNumber- Dummy Test', (done) =>
   })
 });
 
-test('should return data from postgreSQL by recordNumber', (done) => {
+test('should return data from postgreSQL by productId, bottom 1% of the database', (done) => {
   return knex('relatedproducts')
-  .where({recordnumber: 9999090})
-  .then(result => {
-    expect(result).toBeTruthy();
-  })
-  .then(() => {
-    done();
-  })
-});
-
-test('should return data from postgreSQL by productId', (done) => {
-  return knex('relatedproducts')
-    .where({productid: '64ba9ece-d695-4277-84ea-0ddac7cb9129'})
+    .where({productid: 'fb0b1ac1-d549-4813-9284-6d85f5996939'})
     .then(result => {
       expect(result).toBeTruthy();
     })
     .then(() => {
-      knex.destroy()
-    })
-    .then(() => {
       done();
     })
+});
+
+test('should return data from postgreSQL by recordNumber, bottom 1% of the database', (done) => {
+  return knex('relatedproducts')
+  .where({recordnumber: 9999089})
+  .then(result => {
+    expect(result).toBeTruthy();
+  })
+  .then(() => {
+    knex.destroy()
+  })
+  .then(() => {
+    done();
+  })
 });
 
 test('should return data from Cassadra by recordNumber- Dummy test', (done) => {
   const query = 'SELECT * FROM sdc.relatedproducts WHERE recordnumber = ?';
-  return client.execute(query, ['9999090'], {prepare: true})
+  return client.execute(query, ['200'], {prepare: true})
     .then(result => {
       expect(result).toBeTruthy();
     })
@@ -66,20 +66,20 @@ test('should return data from Cassadra by recordNumber- Dummy test', (done) => {
     })
 });
 
-test('should return data from Cassadra by recordNumber', (done) => {
-  const query = 'SELECT * FROM sdc.relatedproducts WHERE recordnumber = ?';
-  return client.execute(query, ['9999090'], {prepare: true})
-    .then(result => {
-      expect(result).toBeTruthy();
-    })
-    .then(() => {
-      done();
-    })
-});
-
-test('should return data from Cassadra by productId', (done) => {
+test('should return data from Cassandra by productId, bottom 1% of the database', (done) => {
   const query = 'SELECT * FROM sdc.relatedproducts WHERE productid = ?';
-  return client.execute(query, ['64ba9ece-d695-4277-84ea-0ddac7cb9129'], {prepare: true})
+  return client.execute(query, ['fb0b1ac1-d549-4813-9284-6d85f5996939'], {prepare: true})
+    .then(result => {
+      expect(result).toBeTruthy();
+    })
+    .then(() => {
+      done();
+    })
+});
+
+test('should return data from Cassandra by recordNumber, bottom 1% of the database', (done) => {
+  const query = 'SELECT * FROM sdc.relatedproducts WHERE recordnumber = ?';
+  return client.execute(query, ['9999089'], {prepare: true})
     .then(result => {
       expect(result).toBeTruthy();
     })
@@ -91,110 +91,14 @@ test('should return data from Cassadra by productId', (done) => {
     })
 });
 
-// describe('aws.js', () => {
-//   test('AWS anonymous function does retrieve 15 photos and is from s3 fecquarkalex', () => {
-//     return aws.then(data => {
-//       expect(data.length).toBe(15);
-//       expect(data[0]).toMatch(new RegExp('https://fecquarkalex.s3-us-west-1.amazonaws.com'));
+
+//   test('should return data from Cassandra by recordNumber, bottom 1% of the database', (done) => {
+//     const query = 'SELECT * FROM sdc.relatedproducts WHERE recordnumber = ?';
+//     return client.execute(query, ['9999089'], {prepare: true})
+//     .then(result => {
+//       expect(result).toBeTruthy();
 //     })
-//   })
-// })
-
-
-// describe('faker.js', () => {
-
-//   test('It is an array with 5 objects inside', () => {
-//     return faker.fakerData(data => {
-//       expect(typeof data === 'object');
-//       expect(Array.isArray(data[0])).toBe(true);
+//     .then(() => {
+//       done();
 //     })
-//   })
-
-//   test('Object has correct keys value pairs.', () => {
-//     return faker.fakerData(data => {
-//       let sample = data[0];
-//       expect(Object.keys(sample)).toContain('image')
-//       expect(typeof sample.image === 'string').toBe(true);
-//       expect(Object.keys(sample)).toContain('productTitle')
-//       expect(typeof sample.productTitle === 'string').toBe(true);
-//       expect(Object.keys(sample)).toContain('price')
-//       expect(typeof sample.price === 'number').toBe(true);
-//       expect(Object.keys(sample)).toContain('shippingCost')
-//       expect(typeof sample.shippingCost === 'number').toBe(true);
-//     })
-//   })
-
-// })
-
-
-
-// describe('postgresql.js', () => {
-
-//   mongoose.connect('mongodb://localhost/fecRepo', {useNewUrlParser: true});
-//   let db = mongo.db;
-//   let repo = mongo.repo;
-
-//     //Will have to refactor this.
-//     it ('mongo.retrieve should find records that were inserted', async () => {
-//       await mongo.del();
-//       await mongo.insertOne();
-//       return await mongo.retrieve()
-//       .then(res => {
-//         expect(res.length === 2).toBe(true);
-//         mongo.del()
-//       });
-//     })
-
-
-//     it('mongo.save should save 10 records',async () => {
-//       //const repo = db.collection('repo');
-//       return mongo.save(async result => {
-//         await repo.find({}, (err, res) => {
-//           expect(JSON.stringify(result) == JSON.stringify(res)).toBe(true);
-//         })
-//       })
-//     })
-
-// })
-
-// describe('mongo.js', () => {
-
-//     let connection;
-//     let db;
-
-//     beforeAll(async () => {
-//       connection = await MongoClient.connect(global.__MONGO_URI__, {useNewUrlParser: true})
-//       db = await connection.db(global.__MONGO_DB_NAME__);
-//     })
-
-//     afterAll(async () => {
-//       await connection.close();
-//     })
-
-//     it('mongo.save should save 5 records',async () => {
-//       const repo = db.collection('repo');
-
-//       return mongo.save(async result => {
-//         expect(result.length === 5).toBe(true);
-//       })
-
-//     })
-
-
-//     it ('mongo.retrieve should find records that were inserted', async () => {
-//       const repo = db.collection('repo');
-//       let mock = {'image': 'https://fecquarkalex.s3-us-west-1.amazonaws.com/watch3.jpg', "productTitle": "Mock Data", "price": 200, "shippingCost": 0}
-
-
-//       let inserted = await repo.insertOne(mock);
-//       let find = await repo.find({});
-
-//       console.log(inserted, "\n", find);
-
-//       return mongo.retrieve(async res => {
-//         console.log('res: ', res);
-//         //console.log('inserted: ', inserted.ops);
-//       })
-//     })
-
-// })
+// });
